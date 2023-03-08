36d41093c9b1163b4f9e96cbcd12d5a43a93db44
Bartosz Malec <bartosz.malec@onet.pl>
2023-03-08 21:17:06 +0100

    Added password equality checking

--- src/main/resources/static/js/validator.js
+++ src/main/resources/static/js/validator.js
@@ -1,11 +1,18 @@
 
-        const passStrength = document.getElementById("password_strength");
-        const passInside   = document.getElementById("password_strength_inside");
+        const AuthDataTypeEnum = Object.freeze({ "Password":1, "PasswordRepeat":2, "PasswordLogin": 3, "Email":4, "Terms":5 });
+        const passStrength     = document.getElementById("password_strength");
+        const passInside       = document.getElementById("password_strength_inside");
+        let   previousBarColor = "#D1D1D1";
+
 
         /**
-         * FUNCTION RESPOSIBLE FOR CHECKING
-         * IF INSERTED DATA MATCHES REGEXP
-        */
+         * Checks data entered by user
+         *
+         * Parameters:
+         *  index -> individual number of tooltip on specified page
+         *  type -> AuthDataTypeEnum specifies type of input text
+         *  data -> data passed from user
+         */
         function checkInputData(index, type, data) {
             let complete = false;
             let msg = [];
@@ -22,20 +29,15 @@
              * [!! IMPORTANT !!]
              *  Only very strong password is considered as valid
              */
-            if (type === "password" || type === "passwordRepeat") {
+            if (type == AuthDataTypeEnum.Password || type == AuthDataTypeEnum.PasswordRepeat) {
                 let width = 100;
-                let errors = 0;
-                n   = 4;
+                n = 4;
                 msg = Array(n);
-                
+
                 msg[0] = "<i class='fas fa-check ok'></i> Minimum 8 znaków<br>";
                 msg[1] = "<i class='fas fa-check ok'></i> Duża litera<br>";
                 msg[2] = "<i class='fas fa-check ok'></i> Cyfra<br>";
                 msg[3] = "<i class='fas fa-check ok'></i> Znak specjalny<br>";
-                if (type === "passwordRepeat") {
-                    msg[4] = "<i class='fas fa-check ok'></i> Identyczne hasła<br>";
-                    n++;
-                }
 
                 if (data.length <= 0) {
                     width -= 10;
@@ -43,54 +45,43 @@
 
                 if (data.length < 8) {
                     width -= 15;
-                    errors++;
                     msg[0] = "<i class='fas fa-times error'></i> Minimum 8 znaków<br>";
-                } 
-                
+                }
+
                 if (!data.match(/[A-Z]/)) {
                     width -= 25;
-                    errors++;
                     msg[1] = "<i class='fas fa-times error'></i> Duża litera<br>";
                 }
-                
+
                 if (!data.match(/[0-9]/)) {
                     width -= 25;
-                    errors++;
                     msg[2] = "<i class='fas fa-times error'></i> Cyfra<br>";
                 }
-                
+
                 if (!data.match(/[$&+,:;=?@#|<>.^*()%!-'"\]\[]/)) {
                     width -= 25;
-                    errors++;
                     msg[3] = "<i class='fas fa-times error'></i> Znak specjalny<br>";
                 }
 
-                let pass = document.getElementById("register_data_password").value;
-                let repeatPass = document.getElementById("register_data_password_repeat").value;
-                if ((type === "passwordRepeat" && data !== pass) || (type === "password" && pass !== repeatPass && repeatPass != "")) {
-                    passInside.style.background = "#DC2A03";
-                    msg[4] = "<i class='fas fa-times error'></i> Identyczne hasła<br>";
-                    errors++;
-                }
 
-                if (type === "password" || type === "passwordRepeat" && data === pass) {
+                if (type == AuthDataTypeEnum.Password) {
                     passStrength.style.visibility = "visible";
                     passInside.style.width = width + "%";
 
-                    if (width < 25) passInside.style.background = "#DC2A03";
-                    if (width >= 25 && width < 75) passInside.style.background = "#DC9F03";
-                    if (width >= 75 && width < 100) passInside.style.background = "#1CBA03";
-                    if (width == 100 && errors == 0) passInside.style.background = "var(--color-light-blue)";
-
-                    if (width == 100 && errors == 0) complete = true;
+                    if (width < 25) { passInside.style.background = "#DC2A03"; previousBarColor = "#DC2A03";}
+                    if (width >= 25 && width < 75) { passInside.style.background = "#DC9F03"; previousBarColor = "#DC9F03";}
+                    if (width >= 75 && width < 100) { passInside.style.background = "#1CBA03"; previousBarColor = "#1CBA03";}
+                    if (width == 100) { passInside.style.background = "var(--color-light-blue)"; previousBarColor = "var(--color-light-blue)";}
                 }
+
+                if (width == 100) complete = true;
             }
             
 
             /**
              * Email REGEXP
              */
-            if (type === "email") {
+            if (type == AuthDataTypeEnum.Email) {
                 n   = 1;
                 msg = Array(n);
                 msg[0]  = "<i class='fas fa-times error'></i> Błędny format<br>";
@@ -105,7 +96,7 @@
             /**
              * Password REGEXP - considered when user logs into account
              */
-            if (type === "passwordLogin") {
+            if (type == AuthDataTypeEnum.PasswordLogin) {
                 n   = 1;
                 msg = Array(n);
                 msg[0]  = "<i class='fas fa-times error'></i> Niedozwolone znaki<br>";
@@ -120,7 +111,7 @@
             /**
              * Check if user accepted terms of use
              */
-            if (type === "terms") {
+            if (type == AuthDataTypeEnum.Terms) {
                 n   = 1;
                 msg = Array(n);
                 msg[0]  = "<i class='fas fa-times error'></i> Pole obowiązkowe<br>";
@@ -139,14 +130,30 @@
             return complete;
         }
 
-        function checkInputDataRepeatPass(index, password, repeatPass) {
-
-            let complete = checkInputData(index, "password", repeatPass);
-            if (password === repeatPass) return;
-
-            passInside.style.background = "#DC2A03";
 
 
+
+
+
+
+        /**
+         * Check if password and repeated password are equal
+         */
+        function checkPasswordsEquality() {
+            const passwordsEquality = document.getElementById("passwords_equality");
+            let pass = document.getElementById("register_data_password").value;
+            let repeatPass = document.getElementById("register_data_password_repeat").value;
+            if (pass != repeatPass && pass != "" && repeatPass != "") {
+                passInside.style.background = "#DC2A03";
+                if (!passwordsEquality.classList.contains("active"))
+                    passwordsEquality.classList.add("active");
+
+                return;
+            }
+
+            passInside.style.background = previousBarColor;
+            if (passwordsEquality.classList.contains("active"))
+                passwordsEquality.classList.remove("active");
         }
         
         
@@ -171,10 +178,10 @@
             let correctData = 0; // Count correct data
             
             if (type === "register") {
-                if (checkInputData(0, "email", email)) correctData++;
-                if (checkInputData(1, "password", password))  correctData++;
-                if (checkInputData(2, "passwordRepeat", passwordRepeat))  correctData++;
-                if (checkInputData(3, "terms", terms))  correctData++;
+                if (checkInputData(0, AuthDataTypeEnum.Email, email)) correctData++;
+                if (checkInputData(1, AuthDataTypeEnum.Password, password))  correctData++;
+                if (checkInputData(2, AuthDataTypeEnum.PasswordRepeat, passwordRepeat))  correctData++;
+                if (checkInputData(3, AuthDataTypeEnum.Terms, terms))  correctData++;
 
                 if (correctData != 4) return; // Value '4' means all data is correct
             }
@@ -233,7 +240,14 @@
 
 
 
-
+        /**
+         * Shows tooltip
+         *
+         * Parameters:
+         *  index -> individual number of tooltip on specified page
+         *  text -> tooltip text
+         *  boolean -> should it consist of positive or negative color and icon
+         */
         function showToolTip(index, text, boolean) {
             let message = document.getElementsByClassName("tooltip_text")[index];
             let icon    = document.getElementsByClassName("tooltip_icon")[index];
