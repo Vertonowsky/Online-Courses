51238fffc052799fb814c35e830fa3d0daa636b7
Bartosz Malec <bartosz.malec@onet.pl>
2023-03-08 18:22:45 +0100

    Added new spring configuration.

--- src/main/java/com/example/hello_world/HelloWorldApplication.java
+++ src/main/java/com/example/hello_world/HelloWorldApplication.java
@@ -5,19 +5,28 @@ import com.example.hello_world.persistence.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @Controller
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer {
+
 
     @Autowired
     private CourseRepository courseRepository;
 
-    public static String videoesPath = "http://192.168.1.18/edu/videos/";
+
+    public static String videoesPath = "http://localhost:8080/videos/";
     public static String videoesLocalPath = "D:\\Xampp\\htdocs\\edu\\videos";
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloWorldApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
