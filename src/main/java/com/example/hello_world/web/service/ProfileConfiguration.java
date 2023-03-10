548e12b378dc32ee9d1206ed09c436bb118f4932
Bartosz Malec <bartosz.malec@onet.pl>
2023-03-10 19:07:15 +0100

    Added possibility to return selected profileService

---
+++ src/main/java/com/example/hello_world/web/service/ProfileConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.hello_world.web.service;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class ProfileConfiguration {
+
+    /**
+     * Environment profiles. There are two environments available.
+     * - dev  -> development environment
+     * - prod -> production environment
+     */
+
+    @Bean
+    @Profile("dev")
+    public ProfileService profileServiceDev(){
+        return new ProfileService("dev");
+    }
+
+    @Bean
+    @Profile("prod")
+    public ProfileService profileServiceProd(){
+        return new ProfileService("prod");
+    }
+}
