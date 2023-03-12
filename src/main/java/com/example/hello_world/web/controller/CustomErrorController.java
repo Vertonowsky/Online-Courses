452dfd132700e99326363a57ec9b1fd14b067e31
Bartosz Malec <bartosz.malec@onet.pl>
2023-03-12 10:35:00 +0100

    Handle errors and display them all custom page.

---
+++ src/main/java/com/example/hello_world/web/controller/CustomErrorController.java
@@ -0,0 +1,44 @@
+package com.example.hello_world.web.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ModelAndView handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE); // get error status
+
+        String title       = "Bad Request";
+        int statusCode     = 400;
+        String description = "Strona, która próbujesz odwiedzić jest tymczasowo niedostępna.";
+
+        if (status != null) {
+            statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                title       = "Page not found";
+                description = "Wygląda na to, że strona, której szukasz nie istnieje.";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                title       = "Internal server error";
+                description = "Serwer nie mógł zrealizować Twojego żądania. Prosimy o kontakt jeśli ponownie natrafisz na ten komunikat.";
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                title       = "Permission denied";
+                description = "Wygląda na to, że nie masz dostępu do strony, którą próbujesz odwiedzić.";
+            }
+        }
+
+        model.addAttribute("statusCode", statusCode);
+        model.addAttribute("title", title);
+        model.addAttribute("description",description);
+        return new ModelAndView("error");
+    }
+}
