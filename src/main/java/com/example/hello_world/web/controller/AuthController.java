cc5c0d1786ca81e35e0d606b96f6a61c2caa3d81
Bartosz Malec <bartosz.malec@onet.pl>
2023-03-14 17:18:20 +0100

    Added possibility to return all external login providers with associated URL.

--- src/main/java/com/example/hello_world/web/controller/AuthController.java
+++ src/main/java/com/example/hello_world/web/controller/AuthController.java
@@ -7,9 +7,12 @@ import com.example.hello_world.validation.UserAlreadyExistsException;
 import com.example.hello_world.web.dto.UserDto;
 import com.example.hello_world.web.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +20,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RestController
@@ -28,6 +32,11 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
+    private static String authorizationRequestBaseUri = "oauth2/authorization";
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
 
     @GetMapping("/logowanie")
     public ModelAndView showLoginForm(@RequestParam(value = "registered", required = false) boolean registered) {
@@ -35,6 +44,7 @@ public class AuthController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
             ModelAndView mav = new ModelAndView( "logowanie");
+            mav.addObject("googleUrl", getGoogleLoginUrl());
             mav.addObject("registered", registered);
             return mav;
         }
@@ -49,6 +59,7 @@ public class AuthController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
             ModelAndView mav = new ModelAndView( "logowanie");
+            mav.addObject("googleUrl", getGoogleLoginUrl());
             mav.addObject("registered", false);
             return mav;
         }
@@ -113,9 +124,48 @@ public class AuthController {
     }
 
 
+    @GetMapping("/oauth_login")
+    public ModelAndView getLoginPage(Model model) {
+        model.addAttribute("googleUrl", getGoogleLoginUrl());
+        return new ModelAndView("oauth_login");
+    }
+
+
     @GetMapping("/list-users")
     public Iterable<User> getUsers() {
         return userRepository.findAll();
+    }
+
+
+    private String getGoogleLoginUrl() {
+        Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+        Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository).as(Iterable.class);
+        if (type != ResolvableType.NONE && ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0]))
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+
+        if (clientRegistrations != null) {
+            for (ClientRegistration registration : clientRegistrations) {
+                if (registration.getClientName().equalsIgnoreCase("google"))
+                    return authorizationRequestBaseUri + "/" + registration.getRegistrationId();
+            }
+        }
+
+        return "/";
+
+
+        /*Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+        Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository).as(Iterable.class);
+        if (type != ResolvableType.NONE && ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0]))
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+
+        if (clientRegistrations != null) {
+            for (ClientRegistration registration : clientRegistrations)
+                oauth2AuthenticationUrls.put(registration.getClientName(), authorizationRequestBaseUri + "/" + registration.getRegistrationId());
+        }
+
+        return oauth2AuthenticationUrls;*/
     }
 
 }
