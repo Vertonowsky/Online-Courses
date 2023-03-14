450a54b424500f13bb34d63cc4243671a3b97d0b
Bartosz Malec <bartosz.malec@onet.pl>
2023-03-14 17:12:56 +0100

    Implemented function to return users who registered using the traditional method.

--- src/main/java/com/example/hello_world/persistence/repository/UserRepository.java
+++ src/main/java/com/example/hello_world/persistence/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.hello_world.persistence.repository;
 
+import com.example.hello_world.RegistrationMethod;
 import com.example.hello_world.persistence.model.FinishedTopic;
 import com.example.hello_world.persistence.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.registrationMethod = :registrationMethod")
+    Optional<User> findByEmail(String email, RegistrationMethod registrationMethod);
 
     @Query("SELECT u.finishedTopics FROM User u WHERE u = :user")
     Iterable<FinishedTopic> findFinishedTopics(@Param("user") User user);
