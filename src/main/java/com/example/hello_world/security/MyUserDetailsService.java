6f7646e1e57767a7363c88ed592e6d5a7718334b
Bartosz Malec <bartosz.malec@onet.pl>
2023-03-14 17:14:17 +0100

    Added filtration of users. Now only the ones with registrationMethod = registrationMethod.DEFAULT are being selected.

--- src/main/java/com/example/hello_world/security/MyUserDetailsService.java
+++ src/main/java/com/example/hello_world/security/MyUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.example.hello_world.security;
 
+import com.example.hello_world.RegistrationMethod;
 import com.example.hello_world.persistence.model.User;
 import com.example.hello_world.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +14,23 @@ import java.util.Optional;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    /*
+        This class represents all users who want to login using traditional method. They own a password, that's why they have a property
+        named 'registrationMethod' with a value equal to RegistrationMethod.DEFAULT.
+
+        When traditional validation is being proceeded, Spring Security only looks after users with 'registrationMethod' = RegistrationMethod.DEFAULT.
+
+        This class is responsible for finding those users and returning their UserDetails instance,
+
+     */
+
+
     @Autowired
     UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        //Optional<User> user = userRepository.findByEmail(email);
-        //return new MyUserDetails(email);
-
-        Optional<User> user = userRepository.findByEmail(email);
+        Optional<User> user = userRepository.findByEmail(email, RegistrationMethod.DEFAULT);
 
         user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + email));
 
