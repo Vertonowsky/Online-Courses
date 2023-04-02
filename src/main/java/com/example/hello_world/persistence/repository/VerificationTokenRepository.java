6a4296f7b2ddf6207defab45aedd56ef29ed6832
Bartosz Malec <bartosz.malec@onet.pl>
2023-04-02 12:34:37 +0200

    Add new search method

--- src/main/java/com/example/hello_world/persistence/repository/VerificationTokenRepository.java
+++ src/main/java/com/example/hello_world/persistence/repository/VerificationTokenRepository.java
@@ -1,6 +1,8 @@
 package com.example.hello_world.persistence.repository;
 
+import com.example.hello_world.persistence.model.User;
 import com.example.hello_world.persistence.model.VerificationToken;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
@@ -8,6 +10,9 @@ import java.util.Optional;
 public interface VerificationTokenRepository extends CrudRepository<VerificationToken, Integer> {
 
     Optional<VerificationToken> findById(Integer id);
+
+    @Query("SELECT t FROM VerificationToken t WHERE t.user = :user AND t.valid = :valid")
+    Optional<VerificationToken> findByUserAndValid(User user, boolean valid);
 
     void delete(VerificationToken verificationToken);
 
