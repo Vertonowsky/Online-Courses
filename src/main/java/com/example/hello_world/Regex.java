2b078f2c142d7c98dc81adb9a47c5d57357468f0
Bartosz Malec <bartosz.malec@onet.pl>
2023-04-13 21:39:26 +0200

    Added support for polish text regex. Added .matches() function

--- src/main/java/com/example/hello_world/Regex.java
+++ src/main/java/com/example/hello_world/Regex.java
@@ -1,8 +1,12 @@
 package com.example.hello_world;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum Regex {
 
-    DISCOUNT_PATTERN("[a-zA-Z0-9]*");
+    DISCOUNT_PATTERN("[a-zA-Z0-9]*"),
+    POLISH_TEXT_PATTERN("[a-zA-Z0-9ąĄćĆśŚęĘóÓłŁńŃżŻźŹ ~!@#$%^&*()-_=+'?,.<>\\[\\]{}|]*");
 
 
     private final String pattern;
@@ -13,5 +17,11 @@ public enum Regex {
 
     public String getPattern() {
         return pattern;
+    }
+
+    public boolean matches(String string) {
+        Pattern pattern = Pattern.compile(getPattern());
+        Matcher matcher = pattern.matcher(string);
+        return matcher.matches();
     }
 }
